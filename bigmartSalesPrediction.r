51c25d2bdb66ed1c08302345b15b7df445ae022c
irondrummer92 <ishwarm.bhat@gmail.com>
2016-12-27 18:28:19 +0530

    Create bigmartSalesPrediction.r
    Uses Random Forests prediction to compute sales. Next steps. Try with NNs

---
+++ bigmartSalesPrediction.r
@@ -0,0 +1,134 @@
+setwd("D:/ML/BigMart Sales/")
+
+library(dplyr)
+
+trainRaw <- read.csv("Train_UWu5bXk.csv", stringsAsFactors = F,na.strings = c(" ",""))
+
+sapply(trainRaw, FUN = function(x){return(length(unique(x)))})
+
+# Item_Identifier               Item_Weight          Item_Fat_Content           Item_Visibility 
+# 1559                       416                         5                      7880 
+# Item_Type                  Item_MRP         Outlet_Identifier Outlet_Establishment_Year 
+# 16                      5938                        10                         9 
+# Outlet_Size      Outlet_Location_Type               Outlet_Type         Item_Outlet_Sales 
+# 4                         3                         4                      3493 
+
+
+sapply(trainRaw, FUN = function(x){return(sum(is.na(x)))})
+
+# Item_Identifier               Item_Weight          Item_Fat_Content           Item_Visibility 
+# 0                      1463                         0                         0 
+# Item_Type                  Item_MRP         Outlet_Identifier Outlet_Establishment_Year 
+# 0                         0                         0                         0 
+# Outlet_Size      Outlet_Location_Type               Outlet_Type         Item_Outlet_Sales 
+# 2410                         0                         0                         0 
+
+itemWeights <- trainRaw %>% select(Item_Identifier, Item_Weight) %>% distinct()
+sum(is.na(itemWeights$Item_Weight))
+# 1142
+
+trainNoNA <- itemWeights[which(!is.na(itemWeights$Item_Weight)),]
+itemWeightsMissing <- merge(itemWeights,trainNoNA,by = "Item_Identifier",all.x = T)
+
+# Find out where both values are missing. Such values can be imputed as mean
+missingCounts <- itemWeightsMissing %>% 
+  select(Item_Weight.x,Item_Weight.y) %>% 
+  transform(present_x = is.na(Item_Weight.x)) %>%
+  transform(present_y = is.na(Item_Weight.y)) %>%
+  group_by(present_x,present_y) %>%
+  summarize(counts = n())
+
+itemWeightsMissing$ItemWeightImputed <- ifelse(is.na(itemWeightsMissing$Item_Weight.x),
+                                               yes = itemWeightsMissing$Item_Weight.y,
+                                               no = itemWeightsMissing$Item_Weight.x)
+
+itemWeightsMissing$ItemWeightImputed[is.na(itemWeightsMissing$ItemWeightImputed)] <- 
+  mean(itemWeightsMissing$ItemWeightImputed,na.rm = T)
+
+itemWeightsMissing <- unique(itemWeightsMissing[,c("Item_Identifier", "ItemWeightImputed")])
+
+trainWeightsImputed <- merge(trainRaw,y = itemWeightsMissing,by = "Item_Identifier")
+
+# Treating outlet size column
+# How do outlet sizes compare to outlet type
+table(trainWeightsImputed$Outlet_Type,
+      trainWeightsImputed$Outlet_Size,useNA = "ifany")
+table(trainWeightsImputed$Outlet_Identifier,
+      trainWeightsImputed$Outlet_Size,useNA = "ifany")
+table(trainWeightsImputed$Outlet_Identifier,
+      trainWeightsImputed$Outlet_Type,useNA = "ifany")
+table(trainWeightsImputed$Outlet_Identifier,
+      trainWeightsImputed$Outlet_Location_Type,useNA = "ifany")
+table(trainWeightsImputed$Outlet_Location_Type,
+      trainWeightsImputed$Outlet_Size,useNA = "ifany")
+
+# OUT10 = SMALL
+# OUT17 & OUT45 = SMALL
+# All missing outlet sizes replaced by "Small"
+trainSizesimputed <- trainWeightsImputed
+trainSizesimputed$OutletSizeImputed <- ifelse(is.na(trainSizesimputed$Outlet_Size), 
+                                              "Small",
+                                              trainSizesimputed$Outlet_Size)
+
+table(trainSizesimputed$Item_Fat_Content)
+trainFatClean <- trainSizesimputed
+trainFatClean$ItemFatCleaned <- trainFatClean$Item_Fat_Content
+trainFatClean$ItemFatCleaned[trainFatClean$ItemFatCleaned == "reg"] = "Regular"
+trainFatClean$ItemFatCleaned[trainFatClean$ItemFatCleaned == "low fat"] = "Low Fat"
+trainFatClean$ItemFatCleaned[trainFatClean$ItemFatCleaned == "LF"] = "Low Fat"
+table(trainFatClean$ItemFatCleaned)
+
+trainFinal <- trainFatClean
+
+# Creating the Training and Validation datasets
+allRow <- 1:nrow(trainFinal)
+trn <- sample(x = allRow,size = 0.8 * nrow(trainFinal))
+tst <- allRow[!(allRow %in% trn)]
+colnames(trainFinal)
+dataTrain <- trainFinal[trn,c("ItemWeightImputed",
+                              "ItemFatCleaned",
+                              "Item_Visibility",
+                              "Item_Type",
+                              "Item_MRP",
+                              "OutletSizeImputed",
+                              "Outlet_Establishment_Year",
+                              "Outlet_Location_Type",
+                              "Item_Outlet_Sales")]
+
+dataTrain$ItemFatCleaned <- as.factor(dataTrain$ItemFatCleaned)
+dataTrain$Item_Type <- as.factor(dataTrain$Item_Type)
+dataTrain$OutletSizeImputed <- as.factor(dataTrain$OutletSizeImputed)
+dataTrain$Outlet_Location_Type <- as.factor(dataTrain$Outlet_Location_Type)
+
+
+dataTest <- trainFinal[tst,c("ItemWeightImputed",
+                             "ItemFatCleaned",
+                             "Item_Visibility",
+                             "Item_Type",
+                             "Item_MRP",
+                             "OutletSizeImputed",
+                             "Outlet_Establishment_Year",
+                             "Outlet_Location_Type",
+                             "Item_Outlet_Sales")]
+
+dataTest$ItemFatCleaned <- as.factor(dataTest$ItemFatCleaned)
+dataTest$Item_Type <- as.factor(dataTest$Item_Type)
+dataTest$OutletSizeImputed <- as.factor(dataTest$OutletSizeImputed)
+dataTest$Outlet_Location_Type <- as.factor(dataTest$Outlet_Location_Type)
+
+# Model using randomForests algorithm to predict sales of an item
+library(randomForest)
+
+randomModel <- randomForest(formula = Item_Outlet_Sales ~ ., data = dataTrain)
+
+dataValidation <- dataTest
+dataValidation$predictedSales <- predict(object = randomModel,newdata = dataTest)
+
+# Computing metrics of Fit
+dataValidation$deviation <- abs(dataValidation$Item_Outlet_Sales - dataValidation$predictedSales)
+dataValidation$percentageDeviation <- dataValidation$deviation/dataValidation$Item_Outlet_Sales
+
+# Mean Absolute Deviation
+mean(dataValidation$deviation)
+# %MAPE
+mean(dataValidation$percentageDeviation)
