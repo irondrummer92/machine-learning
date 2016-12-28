638ed8340b265de4a9a73d95d13bd03b67c691f9
irondrummer92 <ishwarm.bhat@gmail.com>
2016-12-28 15:49:31 +0530

    Update bigmartSalesPrediction.R

--- bigmartSalesPrediction.R
+++ bigmartSalesPrediction.R
@@ -1,27 +1,33 @@
+# Code to solve the Bigmart sales problem by analytics vidhya
 setwd("D:/ML/BigMart Sales/")
 
+# Set libraries
 library(dplyr)
+library(randomForest)
 
+# Reading and checking input dataset
 trainRaw <- read.csv("Train_UWu5bXk.csv", stringsAsFactors = F,na.strings = c(" ",""))
-
 sapply(trainRaw, FUN = function(x){return(length(unique(x)))})
 
+##---------------------------------------------------------------------------------------------
 # Item_Identifier               Item_Weight          Item_Fat_Content           Item_Visibility 
 # 1559                       416                         5                      7880 
 # Item_Type                  Item_MRP         Outlet_Identifier Outlet_Establishment_Year 
 # 16                      5938                        10                         9 
 # Outlet_Size      Outlet_Location_Type               Outlet_Type         Item_Outlet_Sales 
 # 4                         3                         4                      3493 
-
+##---------------------------------------------------------------------------------------------
 
 sapply(trainRaw, FUN = function(x){return(sum(is.na(x)))})
 
+##---------------------------------------------------------------------------------------------
 # Item_Identifier               Item_Weight          Item_Fat_Content           Item_Visibility 
 # 0                      1463                         0                         0 
 # Item_Type                  Item_MRP         Outlet_Identifier Outlet_Establishment_Year 
 # 0                         0                         0                         0 
 # Outlet_Size      Outlet_Location_Type               Outlet_Type         Item_Outlet_Sales 
 # 2410                         0                         0                         0 
+##---------------------------------------------------------------------------------------------
 
 itemWeights <- trainRaw %>% select(Item_Identifier, Item_Weight) %>% distinct()
 sum(is.na(itemWeights$Item_Weight))
@@ -38,6 +44,7 @@ missingCounts <- itemWeightsMissing %>%
   group_by(present_x,present_y) %>%
   summarize(counts = n())
 
+# Impute weights for products
 itemWeightsMissing$ItemWeightImputed <- ifelse(is.na(itemWeightsMissing$Item_Weight.x),
                                                yes = itemWeightsMissing$Item_Weight.y,
                                                no = itemWeightsMissing$Item_Weight.x)
@@ -70,6 +77,7 @@ trainSizesimputed$OutletSizeImputed <- ifelse(is.na(trainSizesimputed$Outlet_Siz
                                               "Small",
                                               trainSizesimputed$Outlet_Size)
 
+# Check for item fat content
 table(trainSizesimputed$Item_Fat_Content)
 trainFatClean <- trainSizesimputed
 trainFatClean$ItemFatCleaned <- trainFatClean$Item_Fat_Content
@@ -78,6 +86,7 @@ trainFatClean$ItemFatCleaned[trainFatClean$ItemFatCleaned == "low fat"] = "Low F
 trainFatClean$ItemFatCleaned[trainFatClean$ItemFatCleaned == "LF"] = "Low Fat"
 table(trainFatClean$ItemFatCleaned)
 
+# Final training set
 trainFinal <- trainFatClean
 
 # Creating the Training and Validation datasets
@@ -85,6 +94,7 @@ allRow <- 1:nrow(trainFinal)
 trn <- sample(x = allRow,size = 0.8 * nrow(trainFinal))
 tst <- allRow[!(allRow %in% trn)]
 colnames(trainFinal)
+
 dataTrain <- trainFinal[trn,c("ItemWeightImputed",
                               "ItemFatCleaned",
                               "Item_Visibility",
@@ -95,20 +105,14 @@ dataTrain <- trainFinal[trn,c("ItemWeightImputed",
                               "Outlet_Location_Type",
                               "Item_Outlet_Sales")]
 
-dataTrain$ItemWeightImputed <- (dataTrain$ItemWeightImputed - mean(dataTrain$ItemWeightImputed))/sd(dataTrain$ItemWeightImputed)
-dataTrain$Item_Visibility <- (dataTrain$Item_Visibility - mean(dataTrain$Item_Visibility))/sd(dataTrain$Item_Visibility)
-dataTrain$Item_MRP <- (dataTrain$Item_MRP - mean(dataTrain$Item_MRP))/sd(dataTrain$Item_MRP)
-# dataTrain$Outlet_Establishment_Year <- (dataTrain$Outlet_Establishment_Year - mean(dataTrain$Outlet_Establishment_Year))/sd(dataTrain$Outlet_Establishment_Year)
-maxSales <- max(dataTrain$Item_Outlet_Sales)
-minSales <- min(dataTrain$Item_Outlet_Sales)
-dataTrain$Item_Outlet_Sales <- (dataTrain$Item_Outlet_Sales - minSales)/(maxSales - minSales)
+trainBackup <- dataTrain
 
-
+# Conversion of categorical variables to factors
 dataTrain$ItemFatCleaned <- as.factor(dataTrain$ItemFatCleaned)
 dataTrain$Item_Type <- as.factor(dataTrain$Item_Type)
 dataTrain$OutletSizeImputed <- as.factor(dataTrain$OutletSizeImputed)
 dataTrain$Outlet_Location_Type <- as.factor(dataTrain$Outlet_Location_Type)
-
+dataTrain$Outlet_Establishment_Year <- as.factor(dataTrain$Outlet_Establishment_Year)
 
 dataTest <- trainFinal[tst,c("ItemWeightImputed",
                              "ItemFatCleaned",
@@ -120,20 +124,16 @@ dataTest <- trainFinal[tst,c("ItemWeightImputed",
                              "Outlet_Location_Type",
                              "Item_Outlet_Sales")]
 
-dataTest$ItemWeightImputed <- (dataTest$ItemWeightImputed - mean(dataTest$ItemWeightImputed))/sd(dataTest$ItemWeightImputed)
-dataTest$Item_Visibility <- (dataTest$Item_Visibility - mean(dataTest$Item_Visibility))/sd(dataTest$Item_Visibility)
-dataTest$Item_MRP <- (dataTest$Item_MRP - mean(dataTest$Item_MRP))/sd(dataTest$Item_MRP)
-dataTest$Outlet_Establishment_Year <- (dataTest$Outlet_Establishment_Year - mean(dataTest$Outlet_Establishment_Year))/sd(dataTest$Outlet_Establishment_Year)
+testBackup <- dataTest
 
-
+# Conversion of categorical variables to factors
 dataTest$ItemFatCleaned <- as.factor(dataTest$ItemFatCleaned)
 dataTest$Item_Type <- as.factor(dataTest$Item_Type)
 dataTest$OutletSizeImputed <- as.factor(dataTest$OutletSizeImputed)
 dataTest$Outlet_Location_Type <- as.factor(dataTest$Outlet_Location_Type)
+dataTest$Outlet_Establishment_Year <- as.factor(dataTest$Outlet_Establishment_Year)
 
 # Model using randomForests algorithm to predict sales of an item
-library(randomForest)
-
 randomModel <- randomForest(formula = Item_Outlet_Sales ~ ., data = dataTrain)
 
 dataValidation <- dataTest
@@ -147,39 +147,7 @@ dataValidation$percentageDeviation <- dataValidation$deviation/dataValidation$It
 mean(dataValidation$deviation)
 # %MAPE
 mean(dataValidation$percentageDeviation)
+# RMSE
+sqrt(mean(dataValidation$deviation ^ 2))
 
-
-library(neuralnet)
-
-modelTrain <- model.matrix(  ~ Item_Outlet_Sales + ItemWeightImputed + ItemFatCleaned + Item_Visibility + 
-    Item_Type + Item_MRP + OutletSizeImputed + Outlet_Establishment_Year + 
-    Outlet_Location_Type, data = dataTrain)
-
-dataNames <- colnames(modelTrain)
-dataNames <- gsub(dataNames,pattern = " ",replacement = "")
-nnFormula <- as.formula(paste("Item_Outlet_Sales ~ ", 
-                              paste(dataNames[!dataNames %in% c("Item_Outlet_Sales","(Intercept)")],
-                                    collapse = "+")))
-colnames(modelTrain) <- dataNames
-
-nnModel <- neuralnet(data = modelTrain,
-                     formula = nnFormula, 
-                     hidden = 3,
-                     learningrate = 0.00001,
-                     algorithm = "backprop",
-                     act.fct = "logistic")
-
-plot(nnModel)
-
-dataValidation <- dataTest
-modelTest <- model.matrix(  ~ ItemWeightImputed + ItemFatCleaned + Item_Visibility + 
-                              Item_Type + Item_MRP + OutletSizeImputed + Outlet_Establishment_Year + 
-                              Outlet_Location_Type, data = dataTest)
-
-modelTest <- modelTest[,-1]
-
-modelComputation <- compute(x = nnModel,covariate = modelTest)
-
-# Computing metrics of Fit
-dataValidation$deviation <- abs(dataValidation$Item_Outlet_Sales - dataValidation$predictedSales)
-dataValidation$percentageDeviation <- dataValidation$deviation/dataValidation$Item_Outlet_Sales
+# RMSE - 1141 on test dataset. Might be good if modelled on entire dataset
