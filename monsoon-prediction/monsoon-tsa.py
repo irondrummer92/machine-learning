bc5bfa9d82762a0851477ecdf3b489b71476b9fa
irondrummer92 <ishwarm.bhat@gmail.com>
2017-08-21 21:34:04 +0000

    Updated for JJAS decomposition

--- monsoon-prediction/monsoon-tsa.py
+++ monsoon-prediction/monsoon-tsa.py
@@ -5,11 +5,21 @@ import matplotlib.pyplot as plt
 import pickle as pk
 
 # importing some time series data
-f = open("/home/ishwar/workspace/Data/clim-data/monthly_precip.txt")
+f = open("/home/ishwar/workspace/datasets/clim-data/monthly_precip.txt")
 
 txt = f.readlines()
 # Time series from 190101 to 201412
 precip_ts = np.array([float(mon.replace("\n","")) for mon in txt])
+
+tm_arr = np.empty(len(precip_ts), np.int32)
+tm = np.arange(1901, 2015)
+print tm
+j = 0
+for i in range(len(tm)):
+    tm_arr[j:j+12] = tm[i] * 100 + np.arange(1, 13)
+    j = j + 12
+
+print tm_arr
 
 # remove seasonal fluctuations from the plot
 # Get monthly mean of the data and remove to get anomaly
@@ -17,15 +27,19 @@ prec_mon = precip_ts.reshape(len(precip_ts)/12,12)
 mon_clim = prec_mon.mean(axis = 0)
 
 print(mon_clim)
-prec_anom = precip_ts - np.repeat(mon_clim, len(precip_ts)/12)
 
-x_len = len(precip_ts)
-train_ind = np.int(np.floor(0.8 * x_len))
+# Get only JJAS
+tm_jjas = precip_ts[[t % 100 in [6,7,8,9] for t in tm_arr]]
+print len(tm_jjas)
 
-prec_train = precip_ts[0:train_ind].reshape([train_ind, 1])
-y_train = precip_ts[1:(train_ind+1)].reshape([train_ind, 1])
+# convert to 4D dataset
+prec_jjas = tm_jjas.reshape(len(tm), 4)
 
-prec_test = precip_ts[(train_ind):(x_len-1)].reshape([x_len-train_ind-1, 1])
-y_test = precip_ts[(train_ind+1):x_len].reshape([x_len-train_ind-1,1])
-
-
+# Plot the 4 time series (JJAS)
+lbs = ("June", "July", "August", "September")
+lns = ("ro-", "bs-", "gh-", "mD-")
+for i, ts in enumerate(prec_jjas.T):
+    plt.plot(tm, ts, lns[i], label = lbs[i], linewidth = 1.2, markersize = 1.5, markeredgewidth = 0.)
+plt.xticks(np.arange(1901, 2015, 10))
+plt.legend()
+plt.show()
